49a5d76c852fb6f48005a96a67202097eb4a7c1a
AbinChapagain <95869530+AbinChapagain@users.noreply.github.com>
2024-03-05 17:36:29 +0545

    fuck you

--- index.py
+++ index.py
@@ -1,15 +1,15 @@
-import numpy as np
+from numpy import random
 chars = "`1234567890-=qwertyuiopasdfghjklzxcvbnm"
 i = 0
 m = 0
 password = []
 x = int(input("length of password: "))
 while i < x:
-    xo = np.random.randint(0, len(chars)-1)
+    xo = random.randint(0, len(chars)-1)
     xoo = chars[xo]
     password.append(xoo)
     i += 1
 while m < len(password):
     print(password[m], end="")
     m += 1
-print('done')
+print('\n done')
